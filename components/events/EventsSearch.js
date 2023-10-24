102ef159bb95814c6278f7c5c07e8ee29eb8bf13
Johan Alves <johanvaf@gmail.com>
2023-10-24 14:08:19 -0300

    Fix CSS Flexbox

--- components/events/EventsSearch.js
+++ components/events/EventsSearch.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Button from "../ui/Button";
 import styles from "./EventsSearch.module.css";
 import EventSearchField from "../ui/EventSearchField";
@@ -25,32 +25,6 @@ function EventsSearch(props) {
           <EventDatePicker setDataRange={setDataRange} />
         </div>
       </div>
-      {/* <div className={styles.controls}>
-        <div className={styles.control}>
-          <label htmlFor="year">Year</label>
-          <select ref={yearInputRef} name="" id="year">
-            <option value="2021">2021</option>
-            <option value="2022">2022</option>
-          </select>
-        </div>
-        <div className={styles.control}>
-          <label htmlFor="month">Month</label>
-          <select ref={monthInputRef} name="" id="month">
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-        </div>
-      </div> */}
       <Button>Find Events</Button>
     </form>
   );
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,25 +1,76 @@
 import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 import { getAllEvents } from "../../util/backend";
+import { useEffect, useState } from "react";
 
 function AllEventsPage(props) {
-  const { allEvents } = props;
+  const searchParams = useSearchParams();
+  const searchText = searchParams.get("search");
+  const searchDate = searchParams.get("date");
+  const allEvents = props.allEvents;
+  const [events, setEvents] = useState(allEvents);
   const router = useRouter();
 
   function findEventsHandler(search, dateRange) {
     const searchFilter = search ? `&search=${search}` : "";
-    const dateRangeFilter = dateRange
-      ? `&date=${dateRange[0]}_${dateRange[1]}`
-      : "";
+    const dateRangeFilter =
+      dateRange[0] != null && dateRange[1] != null
+        ? `&date=${dateRange[0]}_${dateRange[1]}`
+        : "";
     const fullPath = `/events/?${searchFilter}${dateRangeFilter}`;
     router.push(fullPath);
   }
 
+  function filterEvents() {
+    if (!searchText && !searchDate) {
+      // Se ambos searchText e searchDate estiverem vazios, exiba todos os eventos
+      return setEvents(allEvents);
+    }
+
+    const filteredEvents = allEvents.filter((evt) => {
+      const eventDate = new Date(evt.date).getTime();
+      if (searchText && searchDate) {
+        // Se searchText e searchDate estiverem presentes, aplique a condição "AND"
+        const [startDateStr, endDateStr] = searchDate.split("_");
+        const startDate = new Date(startDateStr).getTime();
+        const endDate = new Date(endDateStr).getTime();
+        return (
+          (evt.title.includes(searchText) ||
+            evt.location.includes(searchText) ||
+            evt.city.includes(searchText)) &&
+          eventDate >= startDate &&
+          eventDate <= endDate
+        );
+      } else if (searchText) {
+        // Se apenas searchText estiver presente, aplique somente a parte do texto
+        return (
+          evt.title.includes(searchText) ||
+          evt.location.includes(searchText) ||
+          evt.city.includes(searchText)
+        );
+      } else if (searchDate) {
+        const [startDateStr, endDateStr] = searchDate.split("_");
+        const startDate = new Date(startDateStr).getTime();
+        const endDate = new Date(endDateStr).getTime();
+        // Se apenas searchDate estiver presente, aplique somente a data
+        return eventDate >= startDate && eventDate <= endDate;
+      }
+    });
+
+    // Atualize o estado com os eventos filtrados
+    setEvents(filteredEvents);
+  }
+
+  useEffect(() => {
+    filterEvents();
+  }, [searchText, searchDate]);
+
   return (
     <div>
       <EventsSearch onSearch={findEventsHandler} />
-      <EventList items={allEvents} />
+      <EventList items={events} />
     </div>
   );
 }
