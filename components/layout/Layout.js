12621e7bc240ab9192107d655d1c5041c70fbf48
Johan Alves <johanvaf@gmail.com>
2024-01-14 11:29:39 -0300

    speed insights

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,12 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Head from "next/head";
 import MainHeader from "./MainHeader";
 
 function Layout(props) {
   return (
     <>
+      <SpeedInsights />
       <Head>
         <link rel="icon" href="/favicon.png" sizes="any" />
       </Head>
